ae9cadb6b8af1af1d3b744f033fd91d4e87aafea
Vini <a01027177@itesm.mx>
2022-05-11 12:18:03 -0500

    Add python example

---
+++ hello.py
@@ -0,0 +1,11 @@
+"""
+Simple test program
+Vinicio Santoyo
+"""
+def main ():
+    print ("Hello world")
+    for i in range (5):
+        print("Another line")
+        print("Holaaaa")
+
+main()    
